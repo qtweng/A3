8e4ea1d2cdb0ff14cfda3af10e701e5587d0ad6c
qtweng <qitaoweng@gmail.com>
2020-06-26 03:13:02 +1000

    Different consideration of production stages

---
+++ CreateStage.java
@@ -0,0 +1,5 @@
+public class Creator extends Stage {
+    public Creator(String name, Storage prev, Storage next) {
+        super(name, prev, next);
+    }
+}
---
+++ FinishStage.java
@@ -0,0 +1,5 @@
+public class Finish extends Stage{
+    public Finish(String name, Storage prev, Storage next) {
+        super(name, prev, next);
+    }
+}
